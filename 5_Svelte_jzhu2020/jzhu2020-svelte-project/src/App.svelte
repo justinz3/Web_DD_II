5a5d186af6f836a28ca372fba2df34f682a60239
jzhu2020 <jzhu2020@k12.andoverma.us>
2020-06-10 16:58:35 -0400

    Add reactive ContactCard component

--- 5_Svelte_jzhu2020/jzhu2020-svelte-project/src/App.svelte
+++ 5_Svelte_jzhu2020/jzhu2020-svelte-project/src/App.svelte
@@ -1,11 +1,12 @@
 <script>
 	import Navbar from './Navbar.svelte'
+    import ContactCard from "./ContactCard.svelte"
 
     // default variable values
-    let name = "Mii";
-    let jobTitle = "JumpMan";
-    let jobDescription = "I platform";
-    let profileImage = "https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png";
+    let userNameInput = "Mii";
+    let userJobTitleInput = "JumpMan";
+    let userJobDescriptionInput = "I platform";
+    let userImageInput = "https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png";
 </script>
 
 <Navbar />
@@ -14,35 +15,43 @@
 <!-- Contact Card Form -->
 <div class="w3-container w3-cell-row">
     
-    <div class="w3-card-4 w3-cell w3-half">
+    <div class="w3-card-4 w3-cell w3-half w3-padding">
         <div class="w3-container w3-green">
             <h2>Contact Card Input</h2>
         </div>
 
         <form class="w3-container">
             <p>
-                <input class="w3-input w3-hover-cyan" type="text" bind:value="{name}">
-                <label>Name</label></p>
-            <p>     
-                <input class="w3-input w3-hover-cyan" type="text" bind:value="{jobTitle}">
+                <input class="w3-input w3-hover-cyan" type="text" bind:value="{userNameInput}" style="width:50%;">
+                <label>Name</label>
+            </p>
+            <p> 
+                <input class="w3-input w3-hover-cyan" type="text" bind:value="{userJobTitleInput}" style="width:50%;">
                 <label>Job Title</label>
             </p>
 
-            <p>     
-                <input class="w3-input w3-hover-cyan" type="text" bind:value="{jobDescription}">
+            <p> 
+                <input class="w3-input w3-hover-cyan" type="text" bind:value="{userJobDescriptionInput}" style="width:50%;">
                 <label>Job Description</label>
             </p>
 
-            <p>     
-                <input class="w3-input w3-hover-cyan" type="text" bind:value="{profileImage}">
+            <p> 
+                <input class="w3-input w3-hover-cyan" type="text" bind:value="{userImageInput}" style="width:50%;">
                 <label>Profile Image</label>
             </p>
         </form>
     </div>
 
-    <div class="w3-cell w3-half">
+    <div class="w3-cell w3-half w3-padding">
 
-        
+        <ContactCard 
+            
+            userName = {userNameInput}
+            userJobTitle = {userJobTitleInput}
+            userJobDescription = {userJobDescriptionInput}
+            userImage = {userImageInput}
+
+        />
 
     </div>
 </div>
---
+++ 5_Svelte_jzhu2020/jzhu2020-svelte-project/src/ContactCard.svelte
@@ -0,0 +1,27 @@
+
+
+<script>
+    export let userName;
+    export let userJobTitle;
+    export let userJobDescription;
+    export let userImage;
+
+</script>
+
+
+<header class="w3-container w3-cyan">
+    <h1>{userName}</h1>
+    <h3>{userJobTitle}</h3>
+  </header>
+<div class="w3-card-4">
+    <img src={userImage} alt="{userImage}" style="width:50%;">
+    <div class="w3-container w3-center">
+        <p>{userJobDescription}</p>
+    </div>
+</div>
+
+<style>
+
+
+
+</style>
